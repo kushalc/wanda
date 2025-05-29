5a7649424e00fca2fc8aa8d09b5a9d0f91230744
Kushal Chakrabarti <kushalc@gmail.com>
2025-05-28 23:19:04 -0700

    Sweeping baseline.

--- scripts/concept_pruning_sweep.sh
+++ scripts/concept_pruning_sweep.sh
@@ -1,24 +1,30 @@
 #!/bin/bash -e
 
-model="gemma-2-2b"
-sae="20-gemmascope-res-16k"
+# FIXME: Ideally, would use HookedTransformer for both but that'll require nontrivial code changes
+# and that creates risk of bugs. With this, might get (small?) constant shifts but shape should be
+# same.
+base_model="google/gemma-2-2b"
 OUTPUT_DIR="outputs/`date +%Y-%m-%d-%H-%M-%S`"
 
+# baseline
+for prune_method in magnitude wanda; do
+    for sparsity_ratio in 0.500 0.250 0.125 0.064 0.032 0.016 0.008; do
+        python main.py \
+            --model $base_model \
+            --prune_method $prune_method \
+            --sparsity_ratio $sparsity_ratio \
+            --activation_dataset c4 \
+            --eval hallucination zero_shot perplexity \
+            --nsamples_eval 64 \
+            --output_dir $OUTPUT_DIR \
+            --device mps
+    done
+done
+
+hooked_model="gemma-2-2b"
+sae="20-gemmascope-res-16k"
 # model="meta-llama/Llama-3.1-8B"
 # sae="25-llamascope-res-32k"
-
-K=0
-# python -m pdb -c continue main.py \
-python main.py \
-    --model $model \
-    --sae $sae \
-    --nsamples_eval 4 \
-    --prune_method concept_noise \
-    --concept_noise_k $K \
-    --activation_dataset c4 \
-    --output_dir $OUTPUT_DIR \
-    --eval hallucination zero_shot perplexity \
-    --device mps
 
 # FIXME: Commented out since pdb doesn't exit unclean.
 # for K in 0 5 10 20; do
